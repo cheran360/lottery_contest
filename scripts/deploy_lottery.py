e4e56c9a2799356cb5350e60d921d49fc467615a
cheran <cherankumar.548@gmail.com>
2022-04-12 22:16:50 +0530

    project deadend

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -1,6 +1,7 @@
+import time
 from scripts.helpful_scripts import get_account, get_contract, fund_with_link
 from brownie import Lottery, network, config
-import time
+
 
 def deploy_lottery():
     account = get_account()
